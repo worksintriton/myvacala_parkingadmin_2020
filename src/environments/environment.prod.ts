1b272e5ad7e2d506d03b37dae5506a46d55b7297
Hari <worksintriton@gmail.com>
2020-10-13 16:27:42 +0530

    commited

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   // apiUrl: 'http://3.101.31.129:3000/',
   // imageURL: 'http://3.101.31.129:3000/'
 
-  apiUrl: 'http://15.207.51.203:3000/',
-  imageURL: 'http://15.207.51.203:3000/'
+  apiUrl: 'https://myvacala.com/api/',
+  imageURL: 'https://myvacala.com/api/'
   // apiUrl: 'http://localhost:91/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
   // imageURL: 'http://3.101.31.129:3000/'
 
 
-  apiUrl: 'http://15.207.51.203:3000/',
-  imageURL: 'http://15.207.51.203:3000/'
+  apiUrl: 'https://myvacala.com/api/',
+  imageURL: 'https://myvacala.com/api/'
 
   // apiUrl: 'http://localhost:91/'
   // 34.211.194.144:3000
